f5ba2a2493262392c4095cab1e9dab608ae958aa
joeyrivas <joeyrivas@gmail.com>
2020-04-25 15:23:32 -0400

    Chapter 3 work

---
+++ Chapter3/challenge_automatic_anagram_generator.py
@@ -0,0 +1,6 @@
+"""Mimic anagram generators referenced in "Further Reading".
+
+Automatically generate phrase anagrams from input name.
+
+Display subset (eg. first 500) to review"""
+
---
+++ Chapter3/gallic_gambit.py
@@ -0,0 +1,6 @@
+from itertools import permutations
+name = 'vodle'
+perms = [''.join(i) for i in permutations(name)]
+print(len(perms))
+print(perms)
+print(*perms, sep='\n')
